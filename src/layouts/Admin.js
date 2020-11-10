a9ab0c98e75710f17525817ed7f49b7dd5b5ba55
Luis <Ruano>
2020-11-10 00:50:16 -0600

    ADD: layouts passed as functional components

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,22 +1,22 @@
 /*!
 
 =========================================================
-* Argon Dashboard React - v1.1.0
+* Argon Dashboard React Hooks - v1.1.0
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/argon-dashboard-react
 * Copyright 2019 Creative Tim (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
+* Coded by Creative Tim & Luis Ruano
 
 =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 // reactstrap components
 import { Container } from "reactstrap";
 // core components
@@ -26,14 +26,17 @@ import Sidebar from "components/Sidebar/Sidebar.js";
 
 import routes from "routes.js";
 
-class Admin extends React.Component {
-  componentDidUpdate(e) {
+const Admin = (props) => {
+  const mainContentRef = React.createRef();
+  useEffect(() => {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
-    this.refs.mainContent.scrollTop = 0;
-  }
-  getRoutes = routes => {
-    return routes.map((prop, key) => {
+    mainContentRef.current.scrollTop = 0;
+    document.body.classList.remove("bg-default");
+  });
+
+  const getRoutes = (routesParam) => {
+    return routesParam.map((prop, key) => {
       if (prop.layout === "/admin") {
         return (
           <Route
@@ -47,46 +50,43 @@ class Admin extends React.Component {
       }
     });
   };
-  getBrandText = path => {
+  const getBrandText = () => {
     for (let i = 0; i < routes.length; i++) {
       if (
-        this.props.location.pathname.indexOf(
-          routes[i].layout + routes[i].path
-        ) !== -1
+        props.location.pathname.indexOf(routes[i].layout + routes[i].path) !==
+        -1
       ) {
         return routes[i].name;
       }
     }
     return "Brand";
   };
-  render() {
-    return (
-      <>
-        <Sidebar
-          {...this.props}
-          routes={routes}
-          logo={{
-            innerLink: "/admin/index",
-            imgSrc: require("assets/img/brand/argon-react.png"),
-            imgAlt: "..."
-          }}
+  return (
+    <>
+      <Sidebar
+        {...props}
+        routes={routes}
+        logo={{
+          innerLink: "/admin/index",
+          imgSrc: require("assets/img/brand/argon-react.png"),
+          imgAlt: "...",
+        }}
+      />
+      <div className="main-content" ref={mainContentRef}>
+        <AdminNavbar
+          {...props}
+          brandText={getBrandText(props.location.pathname)}
         />
-        <div className="main-content" ref="mainContent">
-          <AdminNavbar
-            {...this.props}
-            brandText={this.getBrandText(this.props.location.pathname)}
-          />
-          <Switch>
-            {this.getRoutes(routes)}
-            <Redirect from="*" to="/admin/index" />
-          </Switch>
-          <Container fluid>
-            <AdminFooter />
-          </Container>
-        </div>
-      </>
-    );
-  }
-}
+        <Switch>
+          {getRoutes(routes)}
+          <Redirect from="*" to="/admin/index" />
+        </Switch>
+        <Container fluid>
+          <AdminFooter />
+        </Container>
+      </div>
+    </>
+  );
+};
 
 export default Admin;
